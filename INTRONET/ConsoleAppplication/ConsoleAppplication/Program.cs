bdf0bdb4d7dc1aab340bd032873b26b8e94167c1
shagun gupta <shagun.gupta@tothenew.com>
2020-02-18 10:05:57 +0530

    updated

---
+++ INTRONET/ConsoleAppplication/ConsoleAppplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleAppplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Newers");
+            int a = 5;
+            Console.WriteLine("the value of a is     "+a);
+        }
+    }
+}
---
+++ INTRONET/VisualBasicApplication/VisualBasicApplication/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello Newers!")
+        Dim a As Integer = 15
+        Console.WriteLine(a)
+
+    End Sub
+End Module
