81036e48f0c7bcff101291af9ace40108b9e620d
shagun gupta <shagun.gupta@tothenew.com>
2020-04-19 20:54:15 +0530

    added

---
+++ UNIT_TEST/UnitTestProject1/Operator/BankAccount.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccounts
+{
+    public class BankAccount
+    {
+        private readonly string m_customerName;
+        private double m_balance;
+
+        private BankAccount() { }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+
+        // to debit amount in account
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            if (amount > 5000)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance -= amount; // intentionally incorrect code
+
+        }
+
+
+        // to credit amount in account
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Shagun Gupta", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+    }
+}
---
+++ UNIT_TEST/UnitTestProject1/Operator/DivideOperator.cs
@@ -0,0 +1,67 @@
+﻿
+using ObjectExtension;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DivideOperators
+{
+    public class DivideOperator
+    {
+        private double? _numericResult;
+        public DivideOperator()
+        {
+        }
+
+
+        // evaluate method to divide 2 numeric numbers
+        public object Evaluate(List<object> data)
+        {
+
+            object result;
+            foreach (object value in data)
+            {
+                if (value.IsNumeric())
+                {
+
+                    if (_numericResult == null)
+                    {
+                        _numericResult = Convert.ToDouble(value);
+                    }
+
+                    else
+                    {
+                        try
+                        {
+                            _numericResult = _numericResult.Value / Convert.ToDouble(value);
+                        }
+                        catch (DivideByZeroException)
+                        {
+                            throw new ArgumentException($"You are trying to divide the number by 0");
+                        }
+                        catch (Exception)
+                        {
+                            throw new ArgumentException($"you r trying to enter wrong values");
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    throw new ArgumentException($"Non numeric or string value encountered");
+                }
+            }
+
+            if (_numericResult != null)
+            {
+                result = _numericResult.Value;
+            }
+            else
+            {
+                throw new ArgumentException($"Incorrect arguments encountered in {nameof(DivideOperator)}");
+            }
+            return result;
+        }
+    }
+}
